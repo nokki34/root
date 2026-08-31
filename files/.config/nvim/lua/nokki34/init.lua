22e99bee0fd68e67ecf65810ad083467ad99185d
nokki34 <->
2026-08-31 21:43:54 +0900

    go indent

--- files/.config/nvim/lua/nokki34/init.lua
+++ files/.config/nvim/lua/nokki34/init.lua
@@ -6,4 +6,9 @@ vim.opt.number = true        -- Show absolute line number on the current line
 vim.opt.relativenumber = true -- Show relative line numbers on all other lines
 vim.opt.termguicolors = true -- Show all colors
 
+-- go
+vim.cmd([[
+  autocmd FileType go setlocal tabstop=4 shiftwidth=4 expandtab
+]])
+
 print("Hello from nokki34")
